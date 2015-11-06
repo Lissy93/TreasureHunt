08ffe87b5beac1d341d2f73fdadebb10dbbb9cc4
Alicia Sykes <sykes.alicia@gmail.com>
2015-11-06 10:12:47 +0000

    New fragment for the hunt summary

---
+++ app/src/main/java/net/as93/treasurehunt/controllers/fragments/HuntSummaryFragment.java
@@ -0,0 +1,66 @@
+package net.as93.treasurehunt.controllers.fragments;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+
+import net.as93.treasurehunt.R;
+import net.as93.treasurehunt.controllers.AddLocationActivity;
+
+
+public class HuntSummaryFragment extends Fragment {
+
+    private String huntName;
+    private static final String ARG_SECTION_NUMBER = "section_number";
+
+
+    public static HuntSummaryFragment newInstance(int sectionNumber) {
+        HuntSummaryFragment fragment = new HuntSummaryFragment();
+        Bundle args = new Bundle();
+        args.putInt(ARG_SECTION_NUMBER, sectionNumber);
+        fragment.setArguments(args);
+        return fragment;
+    }
+
+    public HuntSummaryFragment() {
+        // Required empty public constructor
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+
+        // Fetch the hunt name from intent bundle
+        huntName = getActivity().getIntent().getExtras().getString("huntname");
+
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        View rootView = inflater.inflate(R.layout.fragment_hunt_sumary, container, false);
+
+        // Show the add new leg screen when button is pressed
+        Button btnAddLocation = (Button) rootView.findViewById(R.id.btnAddLocation);
+        btnAddLocation.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                Intent intent = new Intent(getActivity(), AddLocationActivity.class);
+                intent.putExtra("huntname", huntName);
+                intent.putExtra("leg", 1);
+                startActivity(intent);
+            }
+        });
+
+
+
+        return rootView;
+    }
+
+}
